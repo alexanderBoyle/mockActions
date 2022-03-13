ab479ba7eab0db19235e79163dcbff469a829243
ConsoleFriend <alexander.boyle@student.howest.be>
2022-03-13 19:08:14 +0100

    Trying out test

---
+++ Howest.Prog.CoinChop.Infrastructure.Tests/Howest.Prog.CoinChop.Infrastructure.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Howest.Prog.CoinChop.Infrastructure.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
